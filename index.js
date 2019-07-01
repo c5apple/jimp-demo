073853344271b2b0a17036fe18d8031b92182e27
c5apple <c5apple_party@yahoo.co.jp>
2019-07-01 19:13:16 +0900

    dist生成

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const jimp = require('jimp');
+const fs = require('fs');
 
 // 画像圧縮
 const MAX_WIDTH = 975;
 const QUALITY = 60;
-jimp.read('src/sample.jpg').then((lenna) => {
-  lenna
-    .resize(Math.min(MAX_WIDTH, lenna.getWidth()), jimp.AUTO)
-    .quality(QUALITY)
-    .write("src/sample-min.jpg");
 
-}).catch((err) => {
-  throw err;
+fs.readdir('src', (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+    jimp.read('src/' + file).then((lenna) => {
+      lenna
+        .resize(Math.min(MAX_WIDTH, lenna.getWidth()), jimp.AUTO)
+        .quality(QUALITY)
+        .write('dist/' + file);
+
+    }).catch((err) => {
+      throw err;
+    });
+  });
 });
